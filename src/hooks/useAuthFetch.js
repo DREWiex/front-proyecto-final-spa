f870d2ef736499592492ecb38e8a7f569c943652
DREWiex <aleon88@gmail.com>
2023-05-02 09:53:27 +0200

    Documento hook useAuthFetch

--- src/hooks/useAuthFetch.js
+++ src/hooks/useAuthFetch.js
@@ -2,21 +2,31 @@ import { useContext, useEffect } from "react";
 import { UserContext } from "../context/UserContext";
 import { authFetch } from "../api/authFetch";
 
+/**
+ * Hace la llamada al fetch tanto para el login como para el registro del usuario
+ * @function fetchingUser
+ * @async
+ * @param {String} url URL que recibe el fetch
+ * @param {String} method Método que recibe el fetch
+ * @param {Object} body Datos del form que recibe el fetch
+ * @param {Boolean} status Su valor depende del evento 'onSubmit'
+ * @returns El estado 'user', que es el objeto con los datos que recibe del fetch
+ */
 export const useAuthFetch = (url, method, body, status) => {
 
     const { user, setUser } = useContext(UserContext);
 
     const fetchingUser = async () => {
 
-        const fetch = await authFetch(url, method, body);
+        const fetch = await authFetch(url, method, body); // invocación del fetch
 
-        setUser(fetch);
+        setUser(fetch); // sobreescribe el estado con el objeto que recibe del fetch
 
     };
 
     useEffect(() => {
 
-        status && fetchingUser(); // condicional: si 'status' es true, invoca la func del fetch
+        status && fetchingUser(); // condicional: solo invoca al fetch si 'status' ('sent' en 'LoginPage' y 'RegisterPage') es true
 
     }, [body])
 
