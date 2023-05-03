bbf0b31087262f79e656f2d73eb21bcde3eac5c5
DREWiex <aleon88@gmail.com>
2023-05-03 09:53:13 +0200

    Toggle del navbar funcionando

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
+
+    const [toggle, setToggle] = useState(true);
 
 
     return (
@@ -12,15 +15,35 @@ export const NavBar = () => {
                 <div>
 
                     <Link href="/home" className="lora italic"> Studyverse </Link>
-                    <button className="nav-toggle" aria-label="Open menu">
+                    <button
+                        onClick={() => { setToggle(!toggle) }}
+                        className="nav-toggle"
+                        aria-label="Open menu"
+                    >
                         <i id="btn-bars" className="fa-solid fa-bars"></i>
                     </button>
 
-                    <ul>
-                        <li><Link href="#rooms"> Salas de estudio </Link> </li>
-                        <li> <Link href="#reservations"> Mis reservas </Link> </li>
-                        <li> <Link href="#contact"> Contacto </Link> </li>
-                    </ul>
+                    {
+                        toggle ? (
+                            <ul>
+                                <li><Link href="#"> Mi perfil </Link> </li>
+                                <li><Link href="#"> Salas de estudio </Link> </li>
+                                <li> <Link href="#"> Reservas </Link> </li>
+                                <li> <Link href="#"> Contacto </Link> </li>
+                                <li> <Link href="#"> Cerrar sesión </Link> </li>
+                            </ul>
+                        ) : (
+                            <ul className='nav-menu_visible'>
+                                <li><Link href="#"> Mi perfil </Link> </li>
+                                <li><Link href="#"> Salas de estudio </Link> </li>
+                                <li> <Link href="#"> Reservas </Link> </li>
+                                <li> <Link href="#"> Contacto </Link> </li>
+                                <li> <Link href="#"> Cerrar sesión </Link> </li>
+                            </ul>
+                        )
+                    }
+
+
 
                 </div>
 
